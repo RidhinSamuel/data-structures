6fc451bee699530ab2550830bd9d490c8cf32392
RidhinSamuel <ridhinsamuel73@gmail.com>
2024-03-28 01:56:29 +0530

    Completed SLL

--- dsa-Python/Linked List/linkedList.py
+++ dsa-Python/Linked List/linkedList.py
@@ -8,24 +8,23 @@ class LinkedList:
         self.head = None
     # Add a new node at begining
     def insertAtStart(self,num):
-        if self.head==None:
-            node=Node(num)
-            self.head=node
-        else:
             node=Node(num)
             node.next=self.head
             self.head=node
-        return
+            return
     def insertAtEnd(self,num):
-        temp=self.head
-        while temp.next:
-            temp=temp.next
-        node=Node(num)
-        temp.next=node
+        if self.head is None:
+            self.head=Node(num)
+        else:
+            temp=self.head
+            while temp.next:
+                temp=temp.next
+            node=Node(num)
+            temp.next=node
         return
     def insertAtIndex(self,num,index):
         if index==0 or self.head is None:
-            self.insertAtStart(num)
+            self.head=Node(num)
         try:
             temp=self.head
             temp2=None
@@ -33,14 +32,52 @@ class LinkedList:
                 temp2=temp
                 temp=temp.next
                 index-=1
-            print("temp is ",temp)
             node=Node(num,temp)
             temp2.next=node
         except Exception as e:
             print("Wrong Index try Again")
             index=int(input("Enter a valid index "))
             self.insertAtIndex(num,index)
-
+    def  deleteFromStart(self):
+        if self.head is None:
+            print("List is empty")
+        else:
+            temp=self.head
+            self.head=self.head.next
+            del temp
+    def deleteFromEnd(self):
+        if self.head is None:
+            print("List is empty")
+        else:
+            temp2=None
+            temp=self.head
+            while temp.next:
+                temp2=temp
+                temp=temp.next
+            if temp2:
+                temp2.next=None
+            else:
+                self.head=None
+    def  deleteAtIndex(self,index):
+        if self.head is None:
+            print("The list is Empty ")
+        else:
+            try:
+                temp=self.head
+                temp2=None
+                while index :
+                    temp2=temp
+                    temp=temp.next
+                    index-=1
+                if temp2:
+                    print("Before deleting",temp.data)
+                    temp2.next=temp.next
+                else:
+                    self.head=None
+            except Exception as e:
+                print("Wrong Index try Again")
+                index=int(input("Enter a valid index "))
+                self.removeAtIndex(index)
     def printList(self):
         if self.head==None:
             print("List is empty")
@@ -58,7 +95,7 @@ if __name__=='__main__':
     linkedList=LinkedList()
     while True:
         try:
-            print("1.Insert At Beginning\n2.Insert At the End\n3.Insert at particular position\n6.Display List\n7.exit ")
+            print("1.Insert At Beginning\n2.Insert At the End\n3.Insert at particular position\n4.Delete From Start\n5.Delete From End\n6.Delete from a index\n7.Display List\n8.exit ")
             ch=int(input("Enter your choice "))
             if ch==1:
                 num=int(input("Enter then number to insert "))
@@ -70,12 +107,29 @@ if __name__=='__main__':
                 num=int(input("Enter the number "))
                 index=int(input("Enter the index to insert the Node "))
                 linkedList.insertAtIndex(num,index)
-            elif ch==7:
+            elif ch==4:
+                print("Before Deletion list:")
+                linkedList.printList()
+                linkedList.deleteFromStart()
+                print("After Deletion list: ")
+                linkedList.printList()
+            elif ch==5:
+                print("Before Deletion List")
+                linkedList.printList()
+                linkedList.deleteFromEnd()
+                print("After Deletion")
+                linkedList.printList()
+            elif ch==6:
+                index=int(input("Enter the index to delete "))
+                linkedList.deleteAtIndex(index)
+                print("The Linked List after deletion is :")
+                linkedList.printList()
+            elif ch==8:
                 print("Program terminated")
                 break
-            elif ch==6:
+            elif ch==7:
                 linkedList.printList()
             else:
                 print("Wrong Choice Try Again")
-        except:
-            pass
+        except  Exception as e:
+            print("Error Occured Try Again")
